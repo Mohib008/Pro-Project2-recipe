8cf9ab85c7766a5f8174f0c1c70675bb1f5a1910
Mohib Arsala <mohib.0008@gmail.com>
2020-08-30 15:59:57 -0400

    typeError solved, in randomColor project

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-const NUM_BOXES = 32;
+const NUM_BOXES = 40;
+const Box = ({color}) => {
+  const style = {
+    width: '180px',
+    height: '180px',
+    display: 'inline-block',
+    backgroundColor: color, 
+  }
+  return <div style={style} />
+};
 
 
 class App extends Component {
   constructor(props){
     super(props);
-    const boxes = Array(NUM_BOXES).fill().map(this.getRamdomColor, this);
+    const boxes = Array(NUM_BOXES).fill().map(this.getRandomColor, this);
     this.state = {boxes};
-  }
 
-  getRamdomColor() {
-    let randomIndex = Math.floor(Math.random() * this.props.allColors.length)
+  setInterval(() => {
+    const boxes = this.state.boxes.slice();
+    const randIndex = Math.floor(Math.random() * boxes.length);
+    boxes[randIndex] = this.getRandomColor();
+    this.setState({boxes});
+  }, 300);
+}
+
+  getRandomColor() {
+    let colorIndex = Math.floor(Math.random() * this.props.allColors.length)
     return this.props.allColors[colorIndex];
   }
       render(){
+        const boxes = this.state.boxes.map((color, index) => (
+            <Box key={index} color={color} />
+        ))
         return (
           <div className="App">
-          Todo: Shows 24 boxes of colors boxes
+           {boxes}
           </div>
         );
       }
