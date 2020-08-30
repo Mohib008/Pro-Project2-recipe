e3f9fba2c093d95695067e428f3ecc9e85c0ddee
Mohib Arsala <mohib.0008@gmail.com>
2020-08-30 13:38:32 -0400

    new stateless function added

--- src/App.js
+++ src/App.js
@@ -2,66 +2,52 @@ import React, { Component } from 'react';
 
 import './App.css';
 
+const NUM_BOXES = 32;
+
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = {
-      instructors: [
-        {
-          Name: "Mohib",
-          hobbies: ["Coding", "sport", "Problem solving"]
-        }, {
-          Name: "Shakib",
-          hobbies: ["Shopping", "Tech", "Programming"]
-        }, {
-          Name: "Wais",
-          hobbies: ["Learning", "Coding", "Gaming ", "Trading"]
-        }
-      ]
+    const boxes = Array(NUM_BOXES).fill().map(this.getRamdomColor, this);
+    this.state = {boxes};
+  }
+
+  getRamdomColor() {
+    let randomIndex = Math.floor(Math.random() * this.props.allColors.length)
+    return this.props.allColors[colorIndex];
+  }
+      render(){
+        return (
+          <div className="App">
+          Todo: Shows 24 boxes of colors boxes
+          </div>
+        );
+      }
+    }
+  
+    App.defaultProps = {
+      allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+                  "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+                  "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+                  "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+                  "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+                  "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+                  "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+                  "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+                  "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+                  "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+                  "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+                  "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+                  "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+                  "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+                  "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+                  "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+                  "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+                  "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+                  "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+                  "Yellow","YellowGreen"]
     };
-    setTimeout(() => {
-      const randInst = Math.floor(
-        Math.random() *
-        this.state.instructors.length
-      );
-      const hobbyIndex = Math.floor(
-        Math.random() *
-        this.state.instructors[randInst].length
-      );
-      const instructors = this.state.instructors.map((inst, i) => {
-        if(i === randInst) {
-          const hobbies = [ ...inst.hobbies];
-          hobbies.splice(hobbyIndex, 1);
-          return {
-            ...inst, hobbies
-          };
-        }
-        return inst;
-      });
-      this.setState({ instructors });
-    }, 5000);
-  }
 
-
-
-  render() {
-    const instructors = this.state.instructors.map((instructor, index) => (
-      <li key={index}>
-        <h3>{instructor.Name}</h3>
-        <h4>hobbies: {instructor.hobbies.join(", ")}</h4>
-      </li>
-    ));
-
-    return (
-      <div className="instructor">
-        <ul>
-          {instructors}
-        </ul>
-      </div>
-    )
-  }
-}
 
 
 export default App;
--- src/Component/instructor.js
+++ src/Component/instructor.js
@@ -3,58 +3,65 @@ import "./instructor.css";
 
 
 class Instructor extends Component{
-    constructor(props){
+   
+    constructor(props) {
         super(props);
         this.state = {
-            Instructor: [
-                {
-                    Name: "Mohib",
-                    hobbies: ["Coding", "sport", "Problem solving"]
-                },{
-                    Name: "Shakib",
-                    hobbies: ["Shopping", "Tech", "Programming"]
-                },{
-                    Name: "Wais", 
-                    hobbies: ["Learning", "Coding", "Gaming ", "Trading" ]
-                }
-            ]
+          instructors: [
+            {
+              Name: "Mohib",
+              hobbies: ["Coding", "sport", "Problem solving"]
+            }, {
+              Name: "Shakib",
+              hobbies: ["Shopping", "Tech", "Programming"]
+            }, {
+              Name: "Wais",
+              hobbies: ["Learning", "Coding", "Gaming ", "Trading"]
+            }
+          ]
         };
         setTimeout(() => {
-            const randInst = Math.floor(
-                Math.random() *
-                this.state.instructor.length
-            );
-            const hobbyIndex = Math.floor(
-                Math.random() *
-                this.state.instructor[randInst].length
-            );
-            const instructor = this.state.instructor.slice();
-            instructor[randInst] = Object.assign({}, [randInst]);
-            instructor[randInst].habbies = instructor[randInst].habbies.slice();
-            instructor[randInst].habbies.slice(hobbyIndex, 1);
-            this.setState({instructor});
+          const randInst = Math.floor(
+            Math.random() *
+            this.state.instructors.length
+          );
+          const hobbyIndex = Math.floor(
+            Math.random() *
+            this.state.instructors[randInst].length
+          );
+          const instructors = this.state.instructors.map((inst, i) => {
+            if(i === randInst) {
+              const hobbies = [ ...inst.hobbies];
+              hobbies.splice(hobbyIndex, 1);
+              return {
+                ...inst, hobbies
+              };
+            }
+            return inst;
+          });
+          this.setState({ instructors });
         }, 5000);
-    }
-
-  
-
-    render(){
-        const instructor = this.state.instructor.map((instructor, index) => (
-            <li key={index}>
-                <h3>{instructor.Name}</h3>
-                <h4>hobbies: {instructor.hobbies.join(", ")}</h4>
-            </li>
+      }
+    
+    
+    
+      render() {
+        const instructors = this.state.instructors.map((instructor, index) => (
+          <li key={index}>
+            <h3>{instructor.Name}</h3>
+            <h4>hobbies: {instructor.hobbies.join(", ")}</h4>
+          </li>
         ));
-        
-        return(
-            <div className="instructor">
-               <ul>
-                {instructor}
-               </ul>
-            </div>
+    
+        return (
+          <div className="instructor">
+            <ul>
+              {instructors}
+            </ul>
+          </div>
         )
+      }
     }
-}
 
 
 export default Instructor;
